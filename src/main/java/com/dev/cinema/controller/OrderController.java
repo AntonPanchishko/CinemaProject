20e6dc9ff06a0d75b5c76a649f934b75e644c9ab
antonpanchishko <PanchishkoMayor@gmail.com>
2021-02-16 21:34:15 +0200

    32) passed checkstyle

--- src/main/java/com/dev/cinema/controller/OrderController.java
+++ src/main/java/com/dev/cinema/controller/OrderController.java
@@ -8,7 +8,11 @@ import com.dev.cinema.service.mapper.OrderMapper;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/orders")
